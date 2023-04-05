8f74f1d5b29afb9231da56ed9595092f0cdfa4c6
15 - Mayyas Obeidat <mayyasobeidat@gmail.com>
2023-04-05 06:20:13 +0300

    new task

---
+++ sight/sight/Controllers/commentsHomesController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using sight.Models;
+
+namespace sight.Controllers
+{
+    public class commentsHomesController : Controller
+    {
+        private sightEntities db = new sightEntities();
+
+        // GET: commentsHomes
+        public ActionResult Index()
+        {
+            var commentsHomes = db.commentsHomes.Include(c => c.photographer);
+            return View(commentsHomes.ToList());
+        }
+
+        // GET: commentsHomes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            commentsHome commentsHome = db.commentsHomes.Find(id);
+            if (commentsHome == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commentsHome);
+        }
+
+        // GET: commentsHomes/Create
+        public ActionResult Create(int? id)
+        {
+
+            var x = User.Identity.GetUserId();
+            id = db.photographers.FirstOrDefault(a => a.user_id == x).id;
+            ViewBag.photographer_id = id;
+
+
+            //ViewBag.photographer_id = new SelectList(db.photographers, "id", "user_id");
+            return View();
+        }
+
+        // POST: commentsHomes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,photographer_id,comment,is_approved,created_at")] commentsHome commentsHome)
+        {
+            if (ModelState.IsValid)
+            {
+                var x = User.Identity.GetUserId();
+                int iduser = db.photographers.FirstOrDefault(a => a.user_id == x).id;
+                ViewBag.clientid = iduser;
+
+                commentsHome.photographer_id= iduser;
+                commentsHome.created_at = DateTime.Now;
+                db.commentsHomes.Add(commentsHome);
+                commentsHome.is_approved = false;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            //ViewBag.photographer_id = new SelectList(db.photographers, "id", "user_id", commentsHome.photographer_id);
+            return View(commentsHome);
+        }
+
+        // GET: commentsHomes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            commentsHome commentsHome = db.commentsHomes.Find(id);
+            if (commentsHome == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.photographer_id = new SelectList(db.photographers, "id", "user_id", commentsHome.photographer_id);
+            return View(commentsHome);
+        }
+
+        // POST: commentsHomes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,photographer_id,comment,is_approved,created_at")] commentsHome commentsHome)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(commentsHome).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.photographer_id = new SelectList(db.photographers, "id", "user_id", commentsHome.photographer_id);
+            return View(commentsHome);
+        }
+
+        // GET: commentsHomes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            commentsHome commentsHome = db.commentsHomes.Find(id);
+            if (commentsHome == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commentsHome);
+        }
+
+        // POST: commentsHomes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            commentsHome commentsHome = db.commentsHomes.Find(id);
+            db.commentsHomes.Remove(commentsHome);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- sight/sight/Controllers/photosAdmins1Controller.cs
+++ sight/sight/Controllers/photosAdmins1Controller.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -11,18 +10,18 @@ using sight.Models;
 
 namespace sight.Controllers
 {
-    public class photosAdminsController : Controller
+    public class photosAdmins1Controller : Controller
     {
         private sightEntities db = new sightEntities();
 
-        // GET: photosAdmins
+        // GET: photosAdmins1
         public ActionResult Index()
         {
             var photosAdmins = db.photosAdmins.Include(p => p.photographer).Include(p => p.PhotographyType1);
             return View(photosAdmins.ToList());
         }
 
-        // GET: photosAdmins/Details/5
+        // GET: photosAdmins1/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -37,32 +36,24 @@ namespace sight.Controllers
             return View(photosAdmin);
         }
 
-        // GET: photosAdmins/Create
+        // GET: photosAdmins1/Create
         public ActionResult Create()
         {
-            ViewBag.photographerProfile = new SelectList(db.photographers, "id", "id");
+            //ViewBag.photographerProfile = new SelectList(db.photographers, "id", "id");
             ViewBag.photographerID = new SelectList(db.photographers, "id", "FullName");
             ViewBag.photographyType = new SelectList(db.PhotographyTypes, "TypeID", "TypeName");
             return View();
         }
 
-        // POST: photosAdmins/Create
+        // POST: photosAdmins1/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,photographerProfile,photo,created_at,photographyType,title,photographerName,photographerID")] photosAdmin photosAdmin, HttpPostedFileBase photo)
+        public ActionResult Create([Bind(Include = "id,photographerProfile,photo,created_at,photographyType,title,photographerName,photographerID")] photosAdmin photosAdmin)
         {
             if (ModelState.IsValid)
             {
-                string imgPath = "";
-                if (photo != null)
-                {
-                    imgPath = Path.GetFileName(photo.FileName);
-                    photo.SaveAs(Path.Combine(Server.MapPath("~/assetsUser/img/portfolio/") + photo.FileName));
-                }
-
-                photosAdmin.photo = imgPath;
                 db.photosAdmins.Add(photosAdmin);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -72,10 +63,9 @@ namespace sight.Controllers
             return View(photosAdmin);
         }
 
-        // GET: photosAdmins/Edit/5
+        // GET: photosAdmins1/Edit/5
         public ActionResult Edit(int? id)
         {
-
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
@@ -90,25 +80,15 @@ namespace sight.Controllers
             return View(photosAdmin);
         }
 
-        // POST: photosAdmins/Edit/5
+        // POST: photosAdmins1/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,photographerProfile,photo,created_at,photographyType,title,photographerName,photographerID")] photosAdmin photosAdmin, HttpPostedFileBase photo)
+        public ActionResult Edit([Bind(Include = "id,photographerProfile,photo,created_at,photographyType,title,photographerName,photographerID")] photosAdmin photosAdmin)
         {
-            photosAdmin.photo = Session["photo"].ToString();
-
             if (ModelState.IsValid)
             {
-                string imgPath = "";
-                if (photo != null)
-                {
-                    imgPath = Path.GetFileName(photo.FileName);
-                    photo.SaveAs(Path.Combine(Server.MapPath("~/assetsUser/img/portfolio/") + photo.FileName));
-                }
-
-                photosAdmin.photo = imgPath;
                 db.Entry(photosAdmin).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -118,7 +98,7 @@ namespace sight.Controllers
             return View(photosAdmin);
         }
 
-        // GET: photosAdmins/Delete/5
+        // GET: photosAdmins1/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -133,7 +113,7 @@ namespace sight.Controllers
             return View(photosAdmin);
         }
 
-        // POST: photosAdmins/Delete/5
+        // POST: photosAdmins1/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
